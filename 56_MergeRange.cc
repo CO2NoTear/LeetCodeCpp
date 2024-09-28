2cfc6568b38a5c8af5c76348a7c332a8a54f1c37
CO2NoTear <sqede@outlook.com>
2024-09-29 01:26:31 +0800

    MergeRange完成

--- 56_MergeRange.cc
+++ 56_MergeRange.cc
@@ -1,3 +1,6 @@
+#include <algorithm>
+#include <iostream>
+#include <iterator>
 #include <vector>
 using namespace std;
 
@@ -7,7 +10,40 @@ using namespace std;
  * 如果有，则这个left和左侧的这个大right湮灭。
  */
 
+// [1,3], [2,4], [3,5], []
+
 class Solution {
 public:
-  vector<vector<int>> merge(vector<vector<int>> &intervals) {}
+  vector<vector<int>> merge(vector<vector<int>> &intervals) {
+    vector<vector<int>> sorted_intervals(intervals.begin(), intervals.end());
+    sort(sorted_intervals.begin(), sorted_intervals.end()
+         // ,[](vector<int> vec_a, vector<int> vec_b) {
+         //   return vec_a[0] < vec_b[0];
+         // });
+    );
+    vector<vector<int>> result;
+    for (auto it = sorted_intervals.begin(); it != sorted_intervals.end();
+         ++it) {
+      if (result.empty() || (*it)[0] > result.back()[1]) {
+        result.push_back((*it));
+      } else if ((*it)[0] <= result.back()[1]) {
+        result.back()[1] = max((*it)[1], result.back()[1]);
+      }
+    }
+    return result;
+
+    // for (int i = 0; i < sorted_intervals.size(); ++i) {
+    //   cout
+    // }
+  }
 };
+int main() {
+  vector<vector<int>> intervals = {{2, 6}, {1, 3},  {0, 5},  {1, 4},
+                                   {4, 5}, {8, 10}, {15, 18}};
+  Solution sol;
+  auto merged_intervals = sol.merge(intervals);
+  for (auto iterator : merged_intervals) {
+    cout << "[" << iterator[0] << "," << iterator[1] << "], ";
+  }
+  return 0;
+}
