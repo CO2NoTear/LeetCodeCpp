d28470b27be8f42a23a250f795dc02c92b31df4c
CO2NoTear <sqede@outlook.com>
2024-09-28 14:16:56 +0800

    MergeRange 思路补充

---
+++ 56_MergeRange.cc
@@ -0,0 +1,13 @@
+#include <vector>
+using namespace std;
+
+/*
+ * 思路：left 和
+ * right分别存储，每一个left，去寻找是否有大于自己的right出现在左侧，
+ * 如果有，则这个left和左侧的这个大right湮灭。
+ */
+
+class Solution {
+public:
+  vector<vector<int>> merge(vector<vector<int>> &intervals) {}
+};
