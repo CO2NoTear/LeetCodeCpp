dc17c5a121a8faaaa0f804622aeb6720e41edfbf
CO2NoTear <sqede@outlook.com>
2024-09-30 00:04:03 +0800

    raindrops brute-force TLE for 4 sample

--- 42_Raindrops.cc
+++ 42_Raindrops.cc
@@ -1,37 +1,50 @@
 #include <algorithm>
 #include <iostream>
+#include <map>
 #include <numeric>
+#include <set>
 #include <utility>
 #include <vector>
 using namespace std;
 
-pair<vector<int>::iterator, vector<int>::reverse_iterator>
-find_range(vector<int> &water, int h_val) {
+// pair<vector<int>::iterator, vector<int>::reverse_iterator>
+pair<int, int> find_range(vector<int> &water, int h_val) {
   auto left = water.begin();
   while (left != water.end()) {
-    if (*left == h_val) {
+    if (*left >= h_val) {
       break;
     }
     ++left;
   }
   auto right = water.rbegin();
   while (right != water.rend()) {
-    if (*right == h_val) {
+    if (*right >= h_val) {
       break;
     }
     ++right;
   }
-  // return make_pair(water.end() - left, water.rend() - right);
-  return make_pair(left, right);
+  return make_pair(left - water.begin(), water.rend() - right - 1);
+  // return make_pair(left, right);
 }
 class Solution {
 public:
   int trap(vector<int> &height) {
+    map<int, int> height_cnt;
+    set<int> valid_heights;
+
+    for (auto h_val : height) {
+      height_cnt.emplace(h_val, 0);
+      height_cnt[h_val]++;
+      if (height_cnt[h_val] >= 2) {
+        valid_heights.insert(h_val);
+      }
+    }
     vector<int> water(height);
     int max_height = *max_element(water.begin(), water.end());
-    for (int h_val = 1; h_val <= max_height; ++h_val) {
-      for (auto it = left; it != right.base(); ++it) {
-        *it = h_val;
+    for (int h_val : valid_heights) {
+      auto [left, right] = find_range(water, h_val);
+      for (int i = left; i <= right; ++i) {
+        water[i] = max(water[i], h_val);
       }
       cout << "height at " << h_val << ", water is :\n";
       for (auto water_h : water) {
@@ -44,10 +57,10 @@ public:
   }
 };
 int main() {
-  vector<int> data = {1, 2, 3, 4, 5, 5, 7, 8, 9};
-  auto [left, right] = find_range(data, 5);
-  cout << "index of left is " << *left << " at " << data.end() - left - 1
-       << endl;
-  cout << "index of right is " << *(right - 1) << " at "
-       << data.rend() - right - 1 << endl;
+  vector<int> data = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+  // auto [left, right] = find_range(data, 1);
+  // cout << "index of left is " << data[left] << " at " << left << endl;
+  // cout << "index of right is " << data[right] << " at " << right << endl;
+  Solution sol;
+  cout << "result = " << sol.trap(data) << endl;
 }
